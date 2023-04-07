88a9207eb8d6bceb8f3150bd9d31f455aa570430
blkcor <1029129867@qq.com>
2023-04-08 02:13:16 +0800

    todo:jwt token verify

---
+++ src/main/java/com/chen/security/config/JwtService.java
@@ -0,0 +1,11 @@
+package com.chen.security.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUsername(String token){
+        //TODO:extract userEmail from jwt token
+        return null;
+    }
+}
